b61a17bedb8a37beabd373021b98b945a4ca515b
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-20 17:25:20 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,9 @@
 Rails.application.routes.draw do
-  get 'recipe_foods/index'
-  get 'recipe_foods/show'
-  get 'recipe_foods/new'
-  get 'recipe_foods/create'
-  get 'recipe_foods/update'
-  get 'recipe_foods/destroy'
-  get 'foods/index'
-  get 'foods/show'
-  get 'foods/new'
-  get 'foods/create'
-  get 'foods/update'
-  get 'foods/destroy'
-  get 'recipes/index'
-  get 'recipes/new'
-  get 'recipes/show'
-  get 'recipes/create'
-  get 'recipes/update'
-  get 'recipes/destroy'
-  get 'users/index'
-  get 'users/new'
-  get 'users/show'
-  get 'users/create'
-  get 'users/update'
-  get 'users/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
+  resources :users do
+    resources :foods
+    resources :recipes do
+      resources :recipe_foods
+    end
+  end
 end
