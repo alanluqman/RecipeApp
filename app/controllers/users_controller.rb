390c9a321bc81282ff6bc0cc5e5c5dd251267ef9
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-20 16:25:38 +0300

    Create Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
