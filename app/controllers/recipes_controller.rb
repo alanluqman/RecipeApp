6f3eec26cd2d1d6938317be57e77dbbe65f61e07
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-20 16:25:53 +0300

    Create Recipes controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,19 @@
+class RecipesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/new'
+  get 'recipes/show'
+  get 'recipes/create'
+  get 'recipes/update'
+  get 'recipes/destroy'
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
