110a2bb78fda3bdf1bfb29a0846cb8a2f64285c6
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-21 12:55:06 +0300

    Implement user recipe index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,11 @@
 class RecipesController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
+    @recipes = Recipe.where(public: false, author_id: @user.id )
+  end
+
+  def public_recipes
+    @recipes = Recipe.where(public: true)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "users#index"
   resources :users do
     resources :foods
+    get '/public_recipes', to: 'recipes#public_recipes'
     resources :recipes do
       resources :recipe_foods
     end
