ea34a5b68d91847bc7251ba4a535ac557a78d2c0
GordonOtieno <otienogordon95@gmail.com>
2022-12-21 15:15:56 +0300

    Create form for adding new food and display foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,19 +1,36 @@
 class FoodsController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
+    @foods = Food.where(author_id: @user.id)
   end
 
   def show
   end
 
   def new
+    @user = User.find(params[:user_id])
+
   end
 
   def create
+    # debugger
+    @user = User.find(params[:user_id])
+    new_food = @user.foods.new(food_data)
+    if new_food.save
+      redirect_to user_foods_path(user_id: @user.id)
+    else
+      p 'Food was not created'
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def food_data
+   params.permit(:name,:measurement_unit,:quantity,:price)
   end
 end
