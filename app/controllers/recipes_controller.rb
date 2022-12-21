d5c7198756f6fc84f7da1e8cde52f642291001a5
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-21 17:32:49 +0300

    Implement Recipe controller show and create

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,14 +12,30 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:user_id])
+    @recipe = Recipe.find(params[:id])
+    @ingrediants = RecipeFood.where(recipe_id: @recipe.id)
   end
 
   def create
+    @user = User.find(params[:user_id])
+    new_recipe = @user.recipes.new(params_data)
+    if new_recipe.save
+      redirect_to user_recipes_path(user_id: @user.id)
+    else
+      p 'Recipe does not added successfully !'
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def params_data
+    params.permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
