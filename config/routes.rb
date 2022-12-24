3e0187bcefdfb24dc9226504a2782b84e63cdcc2
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-24 21:26:12 +0300

    Create patch routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     resources :foods, except: [:show]
     get '/public_recipes', to: 'recipes#public_recipes'
     resources :recipes do
+      get '/generate_shopping_list', to: 'recipes#shoping'
+      patch '/make_public', to: 'recipes#make_public'
+      patch '/make_private', to: 'recipes#make_private'
       resources :recipe_foods
     end
   end
