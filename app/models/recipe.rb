c6e4ce35e1a99cfe2478878f75350ea8c119f223
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-12-24 21:21:14 +0300

    Update recipe model association

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id'
+  has_many :foods, through: :recipe_foods
   validates :preparation_time, :cooking_time, :name, presence: { strict: true }
 end
